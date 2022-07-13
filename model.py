9dd20e7c879283d0e7331048eaf689bf8567d2d2
Zukang Liao <zukang.liao@eng.ox.ac.uk>
2022-07-13 13:15:15 +0100

    model url

--- model.py
+++ model.py
@@ -6,6 +6,14 @@ from torchvision import models
 from torchsummary import summary
 import torch.utils.model_zoo as model_zoo
 
+model_urls = {
+    'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
+    'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
+    'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
+    'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
+    'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
+}
+
 class VGGnet(nn.Module):
     def __init__(self, arch, nb_class, pretrained=False):
         super(VGGnet, self).__init__()
